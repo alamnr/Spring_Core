19aa1e97a0579e474381a2e8ff9b3bcfce362915
alamnr <alamnr@gmail.com>
2018-04-13 00:12:47 +0600

    Commit for Tutorial 12

--- src/org/koushik/javabrains/DrawingApp.java
+++ src/org/koushik/javabrains/DrawingApp.java
@@ -16,19 +16,7 @@ public class DrawingApp {
 		Triangle triangle = (Triangle)context.getBean("triangle");
 		triangle.draw();
 		
-		Triangle triangle1 = (Triangle)context.getBean("triangle");
 		
-		// Default bean scope is singleton bean is created once when applicationContext is initialised. Only once per spring container
-		// prototype scope creates new bean each time when getBean()  method is called not when applicationContext is initialised - lazy initialisation
-		// Web-aware context bean scope
-		// Request scope- new bean per servlet request
-		// Session scope - New bean per session
-		// Global Session - New bean per global HTTP session (portlet context) 
-		System.out.println(triangle.toString());
-		System.out.println(triangle1.toString());
-		
-		System.out.println(triangle==triangle1);
-
 	}
 
 }
