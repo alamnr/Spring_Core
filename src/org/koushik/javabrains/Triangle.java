a3c9757eb0be6b26132d9a56d5329732fad5b5c9
alamnr <alamnr@gmail.com>
2018-04-12 14:45:37 +0600

    Commit for Tutorial_06

--- src/org/koushik/javabrains/Triangle.java
+++ src/org/koushik/javabrains/Triangle.java
@@ -3,18 +3,38 @@ package org.koushik.javabrains;
 public class Triangle {
 	
 	private String type;
+	private int height;
 	
 	public String getType() {
 		return type;
 	}
-
-	public void setType(String type) {
-		this.type = type;
+	
+	public int getHeight() {
+		return height;
 	}
 
 
+
+	public Triangle(String type){
+		this.type = type;
+	}
+	
+	public Triangle(int height){
+		this.height = height;
+	}
+	
+	public Triangle(String type, int height){
+		this.type = type;
+		this.height = height;
+	}
+
+	/*public void setType(String type) {
+		this.type = type;
+	}*/
+
+
 	public void draw(){
-		System.out.println(getType() + " Triangle Drawn ");
+		System.out.println(getType() + " Triangle Drawn with height - "+ getHeight());
 	}
 
 }
