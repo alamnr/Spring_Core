8ca3efb139a48147976695f1e4e017b2343907a5
alamnr <alamnr@gmail.com>
2018-04-12 13:52:55 +0600

    Commit for Tutorial_05

--- src/org/koushik/javabrains/Triangle.java
+++ src/org/koushik/javabrains/Triangle.java
@@ -2,8 +2,19 @@ package org.koushik.javabrains;
 
 public class Triangle {
 	
+	private String type;
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
 	public void draw(){
-		System.out.println("Triangle Drawn");
+		System.out.println(getType() + " Triangle Drawn ");
 	}
 
 }
